756b016ab23f8f660835d9929220c3e477784315
lee <CIH_Y2K@outlook.com>
2018-08-30 01:19:28 +0800

    add for test_spider/

---
+++ test_spider/html_downloader.py
@@ -0,0 +1,14 @@
+#coding:utf8
+#import urllib2 #python2
+import urllib.request
+class HtmlDownloader(object):
+    def download(self,url):
+        if url is None:
+            return None
+        
+        #response = urllib2.urlopen(url)
+        response=urllib.request.urlopen(url)
+        if response.getcode() != 200:
+            return None
+        
+        return response.read()
